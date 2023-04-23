0d11c7744737a943d8c39e47b213de0d6775acae
duce3790 <duce3790@gmail.com>
2023-04-23 11:57:55 +0800

    fix model_factory to add custom model

--- cyy_torch_toolbox/model_factory.py
+++ cyy_torch_toolbox/model_factory.py
@@ -31,7 +31,7 @@ __model_info: dict = {}
 def get_model_info() -> dict:
     github_repos = [
         "pytorch/vision:main",
-        "cyyever/torch_models:main",
+        "duce3790/torch_models:main",
         "rwightman/pytorch-image-models:master",
     ]
 
@@ -157,7 +157,7 @@ def get_model(
             elif dataset_collection.dataset_type == DatasetType.Text:
                 model_with_loss_fun = TextModelWithLoss
             # print("!!!!!!!!!!!!!!!!!!!!!     change model")
-            model = CNNMnist()
+            # model = CNNMnist()
             # model = CNNCifar()
             model_with_loss = model_with_loss_fun(
                 model=model,
@@ -223,50 +223,7 @@ class ModelConfig:
                 model_with_loss=model_with_loss, tokenizer=dc.tokenizer
             )
         
-        model = CNNMnist()
+        # model = CNNMnist()
         # model = CNNCifar()
 
         return model_with_loss
-
-class CNNMnist(nn.Module):
-    def __init__(self):
-        super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x, is_feat=False, preact=False):
-        f1_pre = F.max_pool2d(self.conv1(x), 2)
-        f1 = F.relu(f1_pre)
-
-        f2_pre = F.max_pool2d(self.conv2_drop(self.conv2(f1)), 2)
-        f2 = F.relu(f2_pre)
-        
-        x = f2
-        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-
-        return F.log_softmax(x, dim=1)
-
-class CNNCifar(nn.Module):
-    def __init__(self):
-        super(CNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return F.log_softmax(x, dim=1)
